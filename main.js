e42b5d3da649485dab1f7dee91cb1f7c6810b253
Foster13 <muhmdfaza@gmail.com>
2024-11-21 17:04:37 +0700

    next code

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const age = 10;
 const name = "Dicoding";
 console.log(`Aku ${name}, umurku ${age} tahun.`);
+
+//1. comment
+// Teks ini akan diabaikan oleh interpreter
+console.log("Hai, Readers!");
+console.log("Hai, JavaScript!");
+// console.log('Hai, Dicoding!');
+
+//2. comment
+/*
+ * TODO
+ * 1. Buatlah variabel bernama `PI` dan isikan dengan nilai 3.14
+ * 2. Cetak nilai variabel PI di terminal menggunakan console.log
+ */
+
+const PI = "3.14";
+console.log(PI);
